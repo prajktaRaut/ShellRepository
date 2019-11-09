14c33c789ca33243e8533e3dac50aae6de9ac5e8
prajktaRaut <53298633+prajktaRaut@users.noreply.github.com>
2019-11-09 17:18:57 +0530

    update program employeefinal

--- employeefinal.sh
+++ employeefinal.sh
@@ -6,11 +6,12 @@ x=$(( RANDOM % 2 ))
 if [ $x == 0 ]; 
 then
 	workingHour=6;
+	wage=$(( $ratePerHour*$workingHour ))
+	echo $wage
 	echo "employee present"
 else 
 then
 	workingHour=0;
 	echo "employee not present"
 fi
-	wage=$(( $ratePerHour*$workingHour ))
-	echo $wage
+	
