f1cdd0a2cf676e0ee932c36e5bd77a6b92e9a1aa
prajktaRaut <prajktasraut16@gmail.com>
2019-11-11 17:44:07 +0530

    solve the given equation

--- ArithmaticOperation.sh
+++ ArithmaticOperation.sh
@@ -5,13 +5,22 @@ read -p "Enter First Input" x
 read -p "Enter Second Input" y
 read -p "Enter third Input" z
 
-	function calculateEquation
+	function sumOfTwoNumber
 	{
-		sum=$(($1 + $2 * $3))
+		sum=$(($1 + $2))
 		echo $sum
 
 	}
-		result="$(calculateEquation $(($x)) $(($y) $(($z))))"
+	
+	function multiplicationOfTwoNumber
+	{
+		mul=$(($1 * $2))
+		result="$(sumOfTwoNumber $(($mul)) $(($x)))"
+		echo $result
 
-		echo "Result of given equation is " $result
+	}
+
+		equationResult="$(multiplicationOfTwoNumber $(($y)) $(($z)))"
+
+		echo "Result of given equation is " $equationResult
 
