daf7144cb9fedfd92dfbe64bedb0f5ddf5a974bf
prajktaRaut <prajktasraut16@gmail.com>
2019-11-11 16:35:30 +0530

    Arithmatic operation is done

---
+++ ArithmaticOperation.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash -x
+
+read -p "Enter First Input" x
+read -p "Enter Second Input" y
+read -p "Enter third Input" z
+
+echo "first input is " $x
+echo "Second input is " $y
+echo "Third input is " $z
