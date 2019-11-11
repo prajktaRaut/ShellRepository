9888ffaa6b12932066458ec7ee5c4c7ac67dcf2b
prajktaRaut <prajktasraut16@gmail.com>
2019-11-11 11:35:57 +0530

    workingHours using function

---
+++ function.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+function myfun()
+{
+	echo $1
+}
+
+result="$( myfun $((RANDOM%2)) )"
+if [ $result -eq 1 ];
+then 
+echo "success"
+else
+echo "failure"
+fi
---
+++ functiondemo.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash  -x
+
+ratePerHour=8;
+isPartTime=2;
+isFullTime=1;
+sum=0;
+
+function myfunction
+{
+echo "number of working hours "$1;
+}
+
+       
+	checker=$(( RANDOM%3 ))
+	case $checker in 
+	$isFullTime)
+        	workingHour=6
+		;;
+	$isPartTime)
+        	workingHour=3
+       		;;
+	*)
+        	workingHour=0
+        	;;
+	esac
+	        result="$( myfunction $(( $workingHour )))"
+
+	echo $result
+
