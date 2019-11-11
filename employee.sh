d0c65bd3c3809a76907d68f37cb7ed931f8e7651
prajktaRaut <prajktasraut16@gmail.com>
2019-11-11 15:39:45 +0530

    Program using array

--- employee.sh
+++ employee.sh
@@ -1,29 +1,45 @@
-#!/bin/bash  -x
 
-ratePerHour=8;
-isPartTime=2;
+#!/bin/bash -x
+
 isFullTime=1;
-sum=0;
+isPartTime=2;
+ratePerHrs=100;
+function totalHrsFunction() {
+       empCheck=$(( RANDOM % 3))
+  case $empCheck in
+       $isFullTime)
+         empHrs=8;;
+       $isPartTime )
+         empHrs=4;;
 
-function myfunction
-{
-echo "number of working hours "$1;
+      *)
+        empHrs=0;;
+  esac  
+  totalHrs=$(perDaySalaryFunction $((empHrs)))
+       echo $totalHrs
+ }
+
+function perDaySalaryFunction() {
+       #for (( counter=1; counter<=20; counter++))
+       #do
+               perDaySalary=$(( $1 * $ratePerHrs ))
+        echo $perDaySalary
+       #done
 }
+for (( counter=1; counter<=20; counter++))
+do
+ 
+totalResult=$(totalHrsFunction)
+       totalSalary[((counter))]=$totalResult
+monthlySalary=0;
+echo $monthlySalary
+for elem in ${totalSalary[@]}; do
+       echo $elem
+   monthlySalary=$(( $monthlySalary + $elem ))
+done
+done
+  echo $monthlySalary
 
-       
-	checker=$(( RANDOM%3 ))
-	case $checker in 
-	$isFullTime)
-        	workingHour=6
-		;;
-	$isPartTime)
-        	workingHour=3
-       		;;
-	*)
-        	workingHour=0
-        	;;
-	esac
-	        result="$( myfunction $(( $workingHour )))"
 
-	echo $result
 
+ 
